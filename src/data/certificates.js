d7e523ba3531e8d3715ebaa50ec29a78c443c7ed
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2023-08-02 12:05:07 -0300

    [mudando para github]

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -4,119 +4,119 @@ export const certificates =
             "id": 1,
             "name": "VUE3 EXPLORANDO O FRAMEWORK",
             "href": "https://cursos.alura.com.br/certificate/3f68c4d5-dfc5-4c93-b185-fa1f85e04a45",
-            "imageSrc": "src/Images/vue3.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/vue3.png?raw=true",
             "imageAlt": "Certificado de vue 3"
         },
         {
             "id": 2,
             "name": "JAVASCRIPT: PROGRAMANDO A ORIENTAÇÃO A OBJETOS",
             "href": "https://cursos.alura.com.br/certificate/384e4ea0-7d46-4eb1-93a5-c17e6496902b",
-            "imageSrc": "src/Images/poo.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/poo.png?raw=true",
             "imageAlt": "Orientação a objetos"
         },
         {
             "id": 3,
             "name": "FLUTTER: CRIANDO UM APP",
             "href": "https://cursos.alura.com.br/certificate/1940470f-95bd-45ca-8f88-9ea6d407f35f",
-            "imageSrc": "src/Images/criandoUmApp.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/CriandoUmApp.png?raw=true",
             "imageAlt": "Flutter Criando um app"
         },
         {
             "id": 4,
             "name": "SCRUM: AGILIDADE EM SEU PROJETO",
             "href": "https://cursos.alura.com.br/certificate/f93f76d5-3675-4642-ab75-77eb5d8d87c3",
-            "imageSrc": "src/Images/scrum.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/scrum.png?raw=true",
             "imageAlt": "Scrum"
         },
         {
             "id": 5,
             "name": "PERSISTÊNCIA COM FLUTTER: CRIE UM APP COM ARMAZENAMENTO INTERNO",
             "href": "https://cursos.alura.com.br/certificate/ad0e0be5-4d69-420e-b5f9-df63bc22785e",
-            "imageSrc": "src/Images/persistencia.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/persistencia.png?raw=true",
             "imageAlt": "persistência de dados"
         },
         {
             "id": 6,
             "name": "FLUTTER: WIDGETS, STATELESS, STATEFUL, IMAGENS E ANIMAÇÕES",
             "href": "https://cursos.alura.com.br/certificate/d69e4e1d-d61b-48e4-ae86-0aa49f28e306",
-            "imageSrc": "src/Images/fluuterWidgtes.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/fluuterWidgtes.png?raw=true",
             "imageAlt": "Widgets com flutter/Stateless/StateFull etc"
         },
         {
             "id": 7,
             "name": "VUE.JS PARTE 1: CONSTRUINDO SINGLE PAGE APPLICATIONS",
             "href": "https://cursos.alura.com.br/certificate/d650d0ae-af86-42eb-90c9-1bb6cbebf488",
-            "imageSrc": "src/Images/vueSpa.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/vueSpa.png?raw=true",
             "imageAlt": "Single page Application com vue"
         },
         {
             "id": 8,
             "name": "FLUTTER COM WEB API: INTEGRANDO SUA APP MOBILE",
             "href": "https://cursos.alura.com.br/certificate/7ed405e3-939a-4738-96b6-b5565d7e7db0",
-            "imageSrc": "src/Images/flutterWebApi.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/flutterWebApi.png?raw=true",
             "imageAlt": "Integrando flutter com web api"
         },
         {
             "id": 9,
             "name": "ORGANIZAÇÃO DE EQUIPES ÁGEIS: OS PAPÉIS EXISTENTES EM UMA EQUIPE",
             "href": "https://cursos.alura.com.br/certificate/e85deaa3-a350-4d34-8cc3-36836b395679",
-            "imageSrc": "src/Images/agile.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/agile.png?raw=true",
             "imageAlt": "Organização da equipe agile"
         },
         {
             "id": 10,
             "name": "FLUTTER: APLICANDO PERSISTÊNCIA DE DADOS",
             "href": "https://cursos.alura.com.br/certificate/4954b3da-8a79-4306-81ce-52c524a9ef7b",
-            "imageSrc": "src/Images/persistenciaDados.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/persistenciaDados.png?raw=true",
             "imageAlt": "persistencia de dados com flutter"
         },
         {
             "id": 11,
             "name": "DART: SINTAXE, COLEÇÕES E DINAMISMO",
             "href": "https://cursos.alura.com.br/certificate/98694ffd-4e60-4d95-a96f-441e71359177",
-            "imageSrc": "src/Images/dart.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/dart.png?raw=true",
             "imageAlt": "dart"
         },
         {
             "id": 12,
             "name": "DART: ENTENDENDO A ORIENTAÇÃO A OBJETOS",
             "href": "https://cursos.alura.com.br/certificate/157841fa-1ad9-4a24-be24-6e300a9af5fd",
-            "imageSrc": "src/Images/dartPoo.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/dartPoo.png?raw=true",
             "imageAlt": "dart orientação a objetos"
         },
         {
             "id": 13,
             "name": "DART: CRIANDO E MANIPULANDO VARIÁVEIS E LISTAS",
             "href": "https://cursos.alura.com.br/certificate/bf2b8ceb-669e-41a9-bbc2-a26d7ce2283d",
-            "imageSrc": "src/Images/dartListas.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/dartListas.png?raw=true",
             "imageAlt": "dart manipulando listas"
         },
         {
             "id": 14,
             "name": "A PARTIR DO ZERO: CRIE PROJETOS EM DART, A LINGUAGEM UTILIZADA NO FLUTTER",
             "href": "https://cursos.alura.com.br/degree/certificate/b4d8b399-91f4-4130-bfd9-2f7e73a88626",
-            "imageSrc": "src/Images/dartFormacao.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/dartFormacao.png?raw=true",
             "imageAlt": "Formação em dart"
         },
         {
             "id": 15,
             "name": "DART: LIDANDO COM EXCEPTIONS E NULL SAFETY",
             "href": "https://cursos.alura.com.br/certificate/ac756ae6-65e5-4fd4-a684-7ecdb7099ef3",
-            "imageSrc": "src/Images/dartNullSafety.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/dartNullSafety.png?raw=true",
             "imageAlt": "Lidando com exeptions e null safety"
         },
         {
             "id": 16,
             "name": "JAVA JRE E JDK: COMPILE E EXECUTE O SEU PROGRAMA",
             "href": "https://cursos.alura.com.br/certificate/7477981a-656e-4083-bbbd-476c7c96f642",
-            "imageSrc": "src/Images/jdk.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/jdk.png?raw=true",
             "imageAlt": "java-Jdk"
         },
         {
             "id": 16,
             "name": "SPRING BOOT 3: DESENVOLVA UMA API REST EM JAVA",
             "href": "https://cursos.alura.com.br/certificate/18686680-175b-4cfe-b331-7d02b4a95f87",
-            "imageSrc": "src/Images/spring.png",
+            "imageSrc": "https://github.com/PedrodoDivino/certificadosAlura/blob/main/spring.png?raw=true",
             "imageAlt": "java-spring"
         }
     ]
